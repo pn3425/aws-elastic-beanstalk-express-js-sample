3bc8f188cdaaed18a26be25c752ff0b0f612d722
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-11-18 23:48:26 +0530

    Added one line to check CI/CD flow

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/test', (req, res) => res.send('CI/CD working!')); // NEW LINE
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
